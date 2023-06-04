9326e82ce2aa639a7d0e2e620e71a07dd7f9d364
John <christophermoreno.jm2@gmail.com>
2023-06-04 09:28:28 +0800

    update

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const port = 8000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("HELLO WORLDsss");
+});
+
+app.listen(port, () => {
+  console.log("server is running");
+});
